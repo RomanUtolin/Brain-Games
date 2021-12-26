d6b7b0b1da0a6debd91203c116f1ec5bd77ced27
kaylide <romautolin@gmail.com>
2021-12-26 22:33:07 +0300

    fix brain-even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,17 +3,13 @@ import prompt
 import random
 
 
-if __name__ == '__main__':
-    main()
-
-
 def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    count = 1
-    while count <= 3:
+    count = 0
+    while count < 3:
         question_number = random.randrange(0, 100)
         print('Question: ' + str(question_number))
         answer = prompt.string('You answer: ')
@@ -24,11 +20,20 @@ def main():
             print('Correct!')
             count += 1
         elif answer == 'yes' and (question_number % 2) != 0:
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.Let's try again, " + str(name) + "!")
-            count = 1
+            count = 5
         elif answer == 'no' and (question_number % 2) == 0:
-            print("'no' is wrong answer ;(. Correct answer was 'yes'.Let's try again, " + str(name) + "!")
-            count = 1
+            count = 6
         elif answer != 'no' or 'yes':
-            print("Please enter correct (yes or no).Let's try again, " + str(name) + "!")
-    print('Congratulations, ' + name + '!')
+            count = 7
+    if count == 3:
+        print('Congratulations, ' + name + '!')
+    elif count == 5:    
+        print(answer + " is wrong answer ;(. Correct answer was 'no'.Let's try again, " + str(name) + "!")
+    elif count == 6:
+        print(answer + " is wrong answer ;(. Correct answer was 'yes'.Let's try again, " + str(name) + "!")
+    elif count == 7:
+        print(answer + " is wrong answer ;(. Correct answer was 'yes' or 'no' .Let's try again, " + str(name) + "!")
+        
+
+if __name__ == '__main__':
+    main()
