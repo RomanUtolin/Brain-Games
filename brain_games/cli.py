2c9f37caa6d892852c653355797e903d8b6ec445
Roman <96241395+RomanUtolin@users.noreply.github.com>
2022-01-14 20:55:28 +0300

    Delete brain_games directory

--- brain_games/cli.py
+++
@@ -1,8 +0,0 @@
-from prompt import string
-
-
-def welcome_user():
-    name = string('May I have your name? ')
-    if name == name:
-        greeting = 'Hello, ' + str(name) + '!'
-        print(greeting)
